899d77c8c1724b440f78ec5009a17975b4431a1b
taocexian <taocexian@jd.com>
2017-05-10 14:04:56 +0800

    222

--- src/com/test/Test2.java
+++ src/com/test/Test2.java
@@ -4,6 +4,15 @@ public class Test2 {
 //2222222
 	//2222222
 	//222dddffffff
+<<<<<<< HEAD
+<<<<<<< HEAD
+	ffff
+=======
 	
 //	sss
+>>>>>>> branch 'master' of https://github.com/taocexian/hello
+=======
+	
+//	sss
+>>>>>>> branch 'master' of https://github.com/taocexian/hello
 }
