5ea614572dfe7dd33609af0087decd75b2bda909
taocexian <taocexian@ZB-PF0FGWPB.360buyAD.local>
2017-05-10 18:19:46 +0800

    10-19

--- src/com/test/Test1.java
+++ src/com/test/Test1.java
@@ -1,4 +1,7 @@
 package com.test;
 
 public class Test1 {
+	public static void main(String[] args) {
+		System.out.println(0+1);
+	}
 }
--- src/com/test/Test2.java
+++ src/com/test/Test2.java
@@ -2,7 +2,6 @@ package com.test;
 
 public class Test2 {
 	public static void main(String[] args) {
-//		22222
-		
+		System.out.println(1+1);
 	}
 }
