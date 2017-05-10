9f480f08cbfff827f2409dc29c1b39a0a793073f
taocexian <taocexian@ZB-PF0FGWPB.360buyAD.local>
2017-05-10 18:35:27 +0800

    1111

--- src/com/test/Test1.java
+++ src/com/test/Test1.java
@@ -3,8 +3,5 @@ package com.test;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println(0+1);
-		System.out.println(0+1);
-		System.out.println(0+1);
-		System.out.println(2+1);
 	}
 }
